e3087859ba0e041dd13eacb3e65c8b01dff23d56
helce <helce@bk.ru>
2025-03-29 16:02:19 +0300

    initial e2k support

--- crypto/async/arch/async_posix.c
+++ crypto/async/arch/async_posix.c
@@ -40,7 +40,11 @@ int async_fibre_makecontext(async_fibre *fibre)
         if (fibre->fibre.uc_stack.ss_sp != NULL) {
             fibre->fibre.uc_stack.ss_size = STACKSIZE;
             fibre->fibre.uc_link = NULL;
+            #ifdef __e2k__
+            makecontext_e2k(&fibre->fibre, async_start_func, 0);
+            #else
             makecontext(&fibre->fibre, async_start_func, 0);
+            #endif
             return 1;
         }
     } else {
@@ -53,6 +57,9 @@ void async_fibre_free(async_fibre *fibre)
 {
     OPENSSL_free(fibre->fibre.uc_stack.ss_sp);
     fibre->fibre.uc_stack.ss_sp = NULL;
+    #ifdef __e2k__
+    freecontext_e2k(&fibre->fibre);
+    #endif
 }
 
 #endif
